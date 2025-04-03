0f869e85ebdd1dfabf5332696221fcfb6307dc4c
Flávio Henrique Barbosa <109081925+flaviohenriquehb777@users.noreply.github.com>
2025-04-03 12:08:04 +0100

    777_07

--- Notebooks/src/config.py
+++ Notebooks/src/config.py
@@ -6,8 +6,8 @@ PASTA_PROJETO = Path(__file__).resolve().parents[2]
 PASTA_DADOS = PASTA_PROJETO / "dados"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
-DADOS_MASTERCARD = PASTA_DADOS / "creditcard.csv.gz"
-DADOS_MASTERCARD_TRATADO = PASTA_DADOS / "creditcard_tratado.parquet"
+DADOS_CREDICARD = PASTA_DADOS / "credicard.csv.gz"
+DADOS_CREDICARD_TRATADO = PASTA_DADOS / "credicard_tratado.parquet"
 
 # DADOS_DIABETES_TRATADOS = PASTA_DADOS / "diabetes.parquet"
 # DADOS_DIABETES_CATEGORIZADOS = PASTA_DADOS / "diabetes_categorizado.parquet"
