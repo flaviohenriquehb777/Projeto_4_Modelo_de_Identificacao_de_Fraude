339cddaa6a43e5944d3bb2d81d23a7c693ab5107
flaviohenriquehb777 <flaviohenriquehb777@outlook.com>
2025-06-27 14:26:31 +0100

    Organiza estrutura do projeto e adiciona novos scripts/modelos

--- Notebooks/src/auxiliares.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def dataframe_coeficientes(coefs, colunas):
-    return pd.DataFrame(data=coefs, index=colunas, columns=["coeficiente"]).sort_values(
-        by="coeficiente"
-    )
